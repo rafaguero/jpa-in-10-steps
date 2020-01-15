67a2e248fda38587cbb10a9e7076b608360e7798
rafael <rafaguero@gmail.com>
2020-01-15 14:20:31 -0700

    made changes

--- src/test/java/com/in28minutes/learning/java/jpain10steps/JpaIn10StepsApplicationTests.java
+++ src/test/java/com/in28minutes/learning/java/jpain10steps/JpaIn10StepsApplicationTests.java
@@ -1,6 +1,6 @@
 package com.in28minutes.learning.java.jpain10steps;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
